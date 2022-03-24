1a11792b593dd3398764678dc17da56861042f5f
Gianlucas Almeida <giandoscp@hotmail.com>
2022-03-24 13:15:42 -0300

    test

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ module.exports = {
   ssr: false,
 
   head: {
-    title: 'FraseologiaWM',
+    title: 'Fraseologia4M',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
